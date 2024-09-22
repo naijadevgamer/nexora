e1bf375b33083f82f946275c80c88dbd7e4286b0
thenaijagamer <sabdullahialaba50@gmail.com>
2024-09-22 11:53:19 +0100

    Implement add to cart functionality

---
+++ app/components/AddToBag.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ProductCart } from "../interface";
+import { urlFor } from "../lib/sanity";
+import { useShoppingCart } from "use-shopping-cart";
+
+const AddToBag = ({
+  currency,
+  name,
+  description,
+  price,
+  image,
+  price_id,
+}: ProductCart) => {
+  const { addItem, handleCartClick } = useShoppingCart();
+
+  const product = {
+    currency,
+    name,
+    description,
+    price,
+    image: urlFor(image).url(),
+    price_id,
+  };
+  return (
+    <Button
+      onClick={() => {
+        addItem(product), handleCartClick();
+      }}
+    >
+      Add to cart
+    </Button>
+  );
+};
+
+export default AddToBag;
