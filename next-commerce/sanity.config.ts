05e82269e2d949cd3ac7ef77ca5a268925ac604b
thenaijagamer <sabdullahialaba50@gmail.com>
2025-09-06 15:23:03 +0100

    Modify sanity config

--- next-commerce/sanity.config.ts
+++ next-commerce/sanity.config.ts
@@ -5,7 +5,7 @@ import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
-  title: 'next-commerce',
+  title: 'nexora-vert',
 
   projectId: '274myesj',
   dataset: 'production',
